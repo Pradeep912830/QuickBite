b85c514f383e32b8ac5129693c9d420aa6f89319
Pradeep912830 <pradeepsaaho432@gmail.com>
2025-05-25 10:26:15 +0530

    Item Added

--- app/src/main/java/com/example/foodorderapp/Fragment/HomeFragment.java
+++ app/src/main/java/com/example/foodorderapp/Fragment/HomeFragment.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -70,9 +71,27 @@ public class HomeFragment extends Fragment {
         popularList.add(new PopularModel(R.drawable.food_three, "Herbal Pancake", "$7", "Add to cart"));
         popularList.add(new PopularModel(R.drawable.food_three, "Salad Bowl", "$5", "Add to cart"));
         popularList.add(new PopularModel(R.drawable.food_three, "Vegan Wrap", "$6", "Add to cart"));
+        popularList.add(new PopularModel(R.drawable.food_three, "Herbal Pancake", "$7", "Add to cart"));
+        popularList.add(new PopularModel(R.drawable.food_three, "Salad Bowl", "$5", "Add to cart"));
+        popularList.add(new PopularModel(R.drawable.food_three, "Vegan Wrap", "$6", "Add to cart"));
+        popularList.add(new PopularModel(R.drawable.food_three, "Herbal Pancake", "$7", "Add to cart"));
+        popularList.add(new PopularModel(R.drawable.food_three, "Salad Bowl", "$5", "Add to cart"));
+        popularList.add(new PopularModel(R.drawable.food_three, "Vegan Wrap", "$6", "Add to cart"));
+        popularList.add(new PopularModel(R.drawable.food_three, "Herbal Pancake", "$7", "Add to cart"));
+        popularList.add(new PopularModel(R.drawable.food_three, "Salad Bowl", "$5", "Add to cart"));
+        popularList.add(new PopularModel(R.drawable.food_three, "Vegan Wrap", "$6", "Add to cart"));
+        popularList.add(new PopularModel(R.drawable.food_three, "Herbal Pancake", "$7", "Add to cart"));
+        popularList.add(new PopularModel(R.drawable.food_three, "Salad Bowl", "$5", "Add to cart"));
+        popularList.add(new PopularModel(R.drawable.food_three, "Vegan Wrap", "$6", "Add to cart"));
+        popularList.add(new PopularModel(R.drawable.food_three, "Herbal Pancake", "$7", "Add to cart"));
+        popularList.add(new PopularModel(R.drawable.food_three, "Salad Bowl", "$5", "Add to cart"));
+        popularList.add(new PopularModel(R.drawable.food_three, "Vegan Wrap", "$6", "Add to cart"));
+
 
         PopularAdapter adapter = new PopularAdapter(popularList);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
         recyclerView.setAdapter(adapter);
     }
+
+
 }
